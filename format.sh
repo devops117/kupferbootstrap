0f881800ac5ec1f028f63b3b14422f66541f3b09
InsanePrawn <insane.prawny@gmail.com>
2021-08-09 00:29:38 +0200

    format.sh: add --check
    Signed-off-by: InsanePrawn <insane.prawny@gmail.com>

--- format.sh
+++ format.sh
@@ -1,15 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-yapf \
-    --recursive \
-    --in-place \
-    --parallel \
-    .
-autoflake \
-    --recursive \
-    --in-place \
-    --remove-unused-variables \
-    --remove-all-unused-imports \
-    --expand-star-imports \
-    --remove-duplicate-keys \
-    .
+yapf_args=('--recursive' '--parallel')
+autoflake_args=('--recursive' '--remove-unused-variables' '--remove-all-unused-imports' '--expand-star-imports' '--remove-duplicate-keys')
+
+format() {
+    yapf "${yapf_args[@]}"  .
+    autoflake "${autoflake_args[@]}" .
+}
+
+
+if [[ "$1" == "--check" ]]; then
+    yapf_args+=('--diff')
+    [[ "$(format | tee /dev/stderr | wc -c)" == "0" ]]
+else
+    yapf_args+=('--in-place')
+    autoflake_args+=('--in-place')
+    format
+fi
