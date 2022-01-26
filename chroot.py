dfce1cf4e0ec05d4404ebe29bc78aac363732370
InsanePrawn <insane.prawny@gmail.com>
2022-01-26 18:14:51 +0100

    chroot.mount_rootfs(): no need to put "loop" into mount options, loopdev is created externally

--- chroot.py
+++ chroot.py
@@ -416,7 +416,7 @@ class Chroot:
                 results[pkg] = self.run_cmd(f'{cmd} {pkg}')
         return results
 
-    def mount_rootfs(self, source_path: str, fs_type: str = None, options: list[str] = ['loop'], allow_overlay: bool = False):
+    def mount_rootfs(self, source_path: str, fs_type: str = None, options: list[str] = [], allow_overlay: bool = False):
         if self.active:
             raise Exception(f'{self.name}: Chroot is marked as active, not mounting a rootfs over it.')
         if not os.path.exists(source_path):
