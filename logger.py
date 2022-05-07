933ffd833cdf284cffe1c531417c58cad9d40253
InsanePrawn <insane.prawny@gmail.com>
2022-05-07 15:27:55 +0200

    logger: add colors

--- logger.py
+++ logger.py
@@ -1,15 +1,20 @@
 import click
+import coloredlogs
 import logging
 import sys
 
 
 def setup_logging(verbose: bool):
+    level_colors = coloredlogs.DEFAULT_LEVEL_STYLES | {'info': {'color': 'magenta', 'bright': True}, 'debug': {'color': 'blue', 'bright': True}}
+    field_colors = coloredlogs.DEFAULT_FIELD_STYLES | {'asctime': {'color': 'white', 'faint': True}}
     level = logging.DEBUG if verbose else logging.INFO
-    logging.basicConfig(
+    coloredlogs.install(
         stream=sys.stdout,
-        format='%(asctime)s %(levelname)s: %(message)s',
-        datefmt='%m/%d/%Y %H:%M:%S',
+        fmt='%(asctime)s %(levelname)s: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
         level=level,
+        level_styles=level_colors,
+        field_styles=field_colors,
     )
     logging.debug('Logging set up.')
 
