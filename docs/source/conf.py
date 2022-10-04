e2e1ba14592bdedc92b1c9c5712da7010df0ff0e
InsanePrawn <insane.prawny@gmail.com>
2022-10-04 15:48:15 +0200

    docs: switch to copper-ish accent colours

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,4 +18,13 @@ html_theme_options = {
     "globaltoc_collapse": True,
     "light_logo": "kupfer-black-transparent.svg",
     "dark_logo": "kupfer-white-transparent.svg",
+    "light_css_variables": {
+        "color-brand-primary": "#882a1a",
+        "color-brand-content": "#882a1a",
+    },
+    "dark_css_variables": {
+        "color-brand-primary": "#eba38d",
+        "color-brand-content": "#eba38d",
+        "color-problematic": "#ff7564",
+    },
 }
